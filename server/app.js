ae34851b957c2e28a0c65bb8ab40f8c07fb1868d
matiasfeliu92 <matumazparrote@gmail.com>
2022-04-21 18:02:12 -0300

    inicio de la app challenge full stack

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+app.set('port', process.env.PORT || 4000)
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+app.listen(app.get('port'), () => {
+    console.log("http://localhost:"+app.get('port'))
+})
