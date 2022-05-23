8510825c7c3c33f3963d27767b2e929c6e91c509
matiasfeliu92 <matumazparrote@gmail.com>
2022-05-23 10:12:40 -0300

    cambio de sintaxis en app.ts para usar typescript

---
+++ server/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+
+const app = express()
+
+// app.set('port', process.env.PORT || 4000)
+
+app.use(morgan('dev'))
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors())
+
+// app.use('/operaciones', require('./routes/operacionesRouter'))
+// app.use('/users', require('./routes/userRouter'))
+
+app.listen(4000, () => {
+    console.log("http://localhost:"+4000)
+})
